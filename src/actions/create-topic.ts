31405810aacfbaed5bace92fb081ee2bbf04b333
Tony Keiser <keisertony@gmail.com>
2025-03-04 11:40:24 -0700

    create topic

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,75 @@
 'use server'
 
-export async function createTopic() {
-  // TODO: Revalidate home page
+import type { Topic } from '@prisma/client'
+import { redirect } from 'next/navigation'
+import { z } from 'zod'
+import { auth } from '@/auth'
+import { db } from '@/db'
+import paths from '@/paths'
+import { revalidatePath } from 'next/cache'
+
+const createTopicSchema = z.object({
+  topic: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, { message: 'Topic must be lowercase and contain only letters and hyphens' }),
+  description: z.string().min(10),
+})
+
+interface CreateTopicFormState {
+  errors: {
+    topic?: string[]
+    description?: string[]
+    _form?: string[]
+  }
+}
+
+export async function createTopic(formState: CreateTopicFormState, formData: FormData): Promise<CreateTopicFormState> {
+  const result = createTopicSchema.safeParse({
+    topic: formData.get('topic'),
+    description: formData.get('description'),
+  })
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    }
+  }
+
+  const session = await auth()
+
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ['You must be signed in to create a topic'],
+      },
+    }
+  }
+
+  let topic: Topic
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.topic,
+        description: result.data.description,
+      },
+    })
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        errors: {
+          _form: [error?.message || ''],
+        },
+      }
+    } else {
+      return {
+        errors: {
+          _form: ['Something went wrong'],
+        },
+      }
+    }
+  }
+
+  revalidatePath('/')
+  redirect(paths.topicsShow(topic.slug))
 }
--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,17 +1,45 @@
+'use client'
+
 import { Input, Button, Textarea, Popover, PopoverContent, PopoverTrigger } from '@heroui/react'
+import { useActionState } from 'react'
 import * as actions from '@/actions'
 
 export default function TopicCreateForm() {
+  const [formState, action] = useActionState(actions.createTopic, {
+    errors: {},
+  })
+
   return (
     <Popover placement="left-start">
       <PopoverTrigger>
         <Button color="primary">Create a Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={actions.createTopic} className="flex flex-col gap-4 p-4 w-80">
+        <form action={action} className="flex flex-col gap-4 p-4 w-80">
           <h3 className="text-lg">Create a Topic</h3>
-          <Input name="topic" label="Topic" labelPlacement="outside" placeholder="Topic"></Input>
-          <Textarea name="description" label="Description" labelPlacement="outside" placeholder="Describe your topic" />
+          <Input
+            name="topic"
+            label="Topic"
+            labelPlacement="outside"
+            placeholder="Topic"
+            isInvalid={!!formState.errors.topic}
+            errorMessage={formState.errors.topic?.join(', ')}
+          ></Input>
+          <Textarea
+            name="description"
+            label="Description"
+            labelPlacement="outside"
+            placeholder="Describe your topic"
+            isInvalid={!!formState.errors.description}
+            errorMessage={formState.errors.description?.join(', ')}
+          />
+
+          {formState.errors._form ? (
+            <div className="px-4 py-2 bg-rose-200 border border-rose-400 text-rose-700 rounded-lg">
+              {formState.errors._form?.join(', ')}
+            </div>
+          ) : null}
+
           <Button type="submit" color="primary">
             Submit
           </Button>
