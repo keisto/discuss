ca7806842d2a946f126e4abf083654a31fd435a4
Tony Keiser <keisertony@gmail.com>
2025-03-05 14:10:49 -0700

    Get Posts

--- src/components/posts/post-list.tsx
+++ src/components/posts/post-list.tsx
@@ -1,9 +1,14 @@
 import type { Post, User, Topic } from '@prisma/client'
 import Link from 'next/link'
 import paths from '@/paths'
+import type { PostWithData } from '@/db/queries/posts'
 
-// TODO: Get list of posts into this component somehow
-export default function PostList() {
+interface Props {
+  fetchData: () => Promise<PostWithData[]>
+}
+
+export default async function PostList({ fetchData }: Props) {
+  const posts = await fetchData()
   const renderedPosts = posts.map((post) => {
     const topicSlug = post.topic.slug
 
---
+++ src/db/queries/posts.ts
@@ -0,0 +1,19 @@
+import type { Post } from '@prisma/client'
+import { db } from '@/db'
+
+export type PostWithData = Post & {
+  user: { name: string | null }
+  topic: { slug: string }
+  _count: { comments: number }
+}
+
+export function fetchPostByTopicSlug(slug: string): Promise<PostWithData[]> {
+  return db.post.findMany({
+    where: { topic: { slug } },
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true } },
+      _count: { select: { comments: true } },
+    },
+  })
+}
