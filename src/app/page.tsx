f68700ca3d5ec66b702740d7477e141e7337758a
Tony Keiser <keisertony@gmail.com>
2025-03-05 01:28:24 -0700

    Show topics list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import TopicCreateForm from '@/components/topics/topic-create-form'
+import TopicList from '@/components/topics/topic-list'
+import { Divider } from '@heroui/react'
 
 export default async function Home() {
   return (
@@ -6,8 +8,13 @@ export default async function Home() {
       <div className="col-span-3">
         <h1 className="text-xl m-2">Top Posts</h1>
       </div>
-      <div>
+      <div className="flex flex-col gap-4 border shadow p-3 rounded-lg">
         <TopicCreateForm />
+        <Divider />
+        <div>
+          <h3 className="text-lg mb-2">Topics</h3>
+          <TopicList />
+        </div>
       </div>
     </div>
   )
