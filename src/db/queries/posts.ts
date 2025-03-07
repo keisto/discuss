b39b088cfaba54c01cb54188fe50af75db628734
Tony Keiser <keisertony@gmail.com>
2025-03-07 08:59:23 -0700

    post on home page

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -19,3 +19,21 @@ export function fetchPostByTopicSlug(slug: string) {
     },
   })
 }
+
+export function fetchTopPosts(): Promise<PostWithData[]> {
+  return db.post.findMany({
+    orderBy: [{ comments: { _count: 'desc' } }],
+    include: {
+      topic: {
+        select: { slug: true },
+      },
+      user: {
+        select: { name: true },
+      },
+      _count: {
+        select: { comments: true },
+      },
+    },
+    take: 5,
+  })
+}
